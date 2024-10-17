6e9c021d86c123c61d58cda187edfd65bb0ff5cc
root <root@jenkins.example.com>
2024-10-17 13:53:56 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+app.get('/', (req, res) => {
+  res.send('Hello World from ECS Fargate!');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
