0de0405f1d6434ec76edb298f2143acc1f10c8b6
sanjaykarde <155716767+sanjaykarde@users.noreply.github.com>
2024-10-22 23:27:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from ECS Fargate!');
+  res.send('Hello World from ECS Fargate!done');
 });
 
 app.listen(port, () => {
